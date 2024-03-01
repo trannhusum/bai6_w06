648e250660f122d9aedf49e3b827637ce0f3ea73
trannhusum <147652638+trannhusum@users.noreply.github.com>
2024-03-01 16:44:53 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai6_w06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Shape();
+            Console.WriteLine(shape);
+            shape = new Shape("red", false);
+            Console.WriteLine(shape);
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("------------------------------------------------------------------------");
+            Circle circle = new Circle();
+            Console.WriteLine(circle);
+            circle = new Circle(3.5);
+            Console.WriteLine(circle);
+            circle = new Circle(3.5, "indigo", false);
+            Console.WriteLine(circle);
+
+            Console.WriteLine("------------------------------------------------------------------------");
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine(rectangle);
+            rectangle = new Rectangle(2.3, 5.8);
+            Console.WriteLine(rectangle);
+            rectangle = new Rectangle(2.5, 3.8, "orange", true);
+            Console.WriteLine(rectangle);
+            
+            Console.WriteLine("------------------------------------------------------------------------");
+
+            Square square = new Square();
+            Console.WriteLine(square);
+            square = new Square(2.3);
+            Console.WriteLine(square);
+            square = new Square(5.8, "yellow", true);
+            Console.WriteLine(square);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,295 @@
+﻿using System;
+
+namespace bai6_w06
+{
+    public class Shape
+    {
+        private string color = "green";
+
+        private bool filled = true;
+
+        public Shape()
+
+        {
+
+        }
+
+        public Shape(String color, bool filled)
+
+        {
+
+            this.color = color;
+
+            this.filled = filled;
+
+        }
+
+        public String GetColor()
+
+        {
+
+            return color;
+
+        }
+
+        public void SetColor(String color)
+
+        {
+
+            this.color = color;
+
+        }
+
+        public bool IsFilled()
+
+        {
+
+            return filled;
+
+        }
+
+        public void SetFilled(bool filled)
+
+        {
+
+            this.filled = filled;
+
+        }
+
+        public override String ToString()
+
+        {
+
+            return "A Shape with color of "
+
+                    +GetColor()
+
+                    + " and "
+
+                    +(IsFilled() ? "filled" : "not filled");
+
+        }
+    }
+    public class Circle : Shape
+
+    {
+
+        private double radius = 1.0;
+
+        public Circle()
+
+        {
+
+        }
+
+        public Circle(double radius)
+
+        {
+
+            this.radius = radius;
+
+        }
+
+        public Circle(double radius, String color, bool filled) : base(color, filled)
+
+        {
+
+            this.radius = radius;
+
+        }
+
+        public double GetRadius()
+
+        {
+
+            return radius;
+
+        }
+
+        public void SetRadius(double radius)
+
+        {
+
+            this.radius = radius;
+
+        }
+
+        public double GetArea()
+
+        {
+
+            return radius * radius * Math.PI;
+
+        }
+
+        public double GetPerimeter()
+
+        {
+
+            return 2 * radius * Math.PI;
+
+        }
+
+        public override String ToString()
+
+        {
+            return "A Circle with radius =" +GetRadius()  + ", which is a subclass of "+base.ToString();
+        }
+
+    }
+    public class Rectangle : Shape
+
+    {
+
+        private double width = 1.0;
+
+        private double length = 1.0;
+
+        public Rectangle()
+
+        {
+
+        }
+
+        public Rectangle(double width, double length)
+
+        {
+
+            this.width = width;
+
+            this.length = length;
+
+        }
+
+        public Rectangle(double width, double length, String color, bool filled) : base(color, filled)
+
+        {
+
+            this.width = width;
+
+            this.length = length;
+
+        }
+
+        public double GetWidth()
+
+        {
+
+            return width;
+
+        }
+
+        public virtual void SetWidth(double width)
+
+        {
+
+            this.width = width;
+
+        }
+
+        public double GetLength()
+
+        {
+
+            return length;
+
+        }
+
+        public virtual void SetLength(double length)
+
+        {
+
+            this.length = length;
+
+        }
+
+        public double GetArea()
+
+        {
+
+            return width * this.length;
+
+        }
+
+        public double GetPerimeter()
+
+        {
+
+            return 2 * (width + this.length);
+
+        }
+
+        public override String ToString()
+
+        {
+
+            return "A Rectangle with width ="+GetWidth()+ " and length =" +GetLength()+ ", which is a subclass of "+base.ToString();
+
+        }
+
+    }
+    public class Square : Rectangle
+
+    {
+
+        public Square()
+
+        {
+
+        }
+
+        public Square(double side) : base(side, side)
+
+        {
+
+        }
+
+        public Square(double side, String color, bool filled) : base(side, side, color, filled)
+
+        {
+
+
+        }
+
+        public double GetSide()
+
+        {
+
+            return GetWidth();
+
+        }
+
+        public void SetSide(double side)
+
+        {
+
+            SetWidth(side);
+
+            SetLength(side);
+
+        }
+
+        public override void SetWidth(double width)
+
+        {
+
+            SetSide(width);
+
+        }
+
+        public override void SetLength(double length)
+
+        {
+
+            SetSide(length);
+
+        }
+
+        public override String ToString()
+
+        {
+            return "A Square with side ="+GetSide()+", which is a subclass of "+base.ToString();
+        }
+
+    }
+}
